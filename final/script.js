7172d78ec8c8d928572ef976ecba59735b856cf3
Seleyna Velasquez <seleyna.velasquez@sjsu.edu>
2025-05-14 20:38:56 -0700

    final

---
+++ final/script.js
@@ -0,0 +1,4 @@
+//document.querySelector('.arrow-link').addEventListener('click', function(e) {
+    //alert("Let's go pick your cabin!");
+    // window.location.href = "/cabins"; // Optional: redirect to another page
+ //});
